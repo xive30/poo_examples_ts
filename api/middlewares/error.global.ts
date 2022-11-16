15dddf9782cebcde3aebc248dfe6a6726cc8cde9
chd <warchd@gmail.com>
2022-11-16 21:42:14 +0100

    feat: add global error middle

---
+++ api/middlewares/error.global.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+ 
+function unexpectedErrorMiddleware(error: Error, request: Request, response: Response, next: NextFunction) {
+  const status = 500;
+  const message = error.message || 'Something went wrong';
+  response
+    .status(status)
+    .send({
+      status,
+      message,
+    })
+}
+ 
+export default unexpectedErrorMiddleware;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors'
 import express from 'express'
 import * as dotenv from 'dotenv'
 import { apiRouter } from '../api/routers/api.router'
+import unexpectedErrorMiddleware from '~~/api/middlewares/error.global'
 
 dotenv.config()
 
@@ -12,6 +13,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use(apiRouter)
+
+app.use(unexpectedErrorMiddleware)
 
 app.listen(process.env.PORT, () => console.log('Running.'))
 
