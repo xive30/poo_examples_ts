1464bb3620d6de2fc7cbacb506af04531e4cb99c
chd <warchd@gmail.com>
2022-11-16 21:20:36 +0100

    feat: add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,17 @@
 import cors from 'cors'
 import express from 'express'
-import { config } from '~/config'
+import * as dotenv from 'dotenv'
 
+dotenv.config()
 
 let helloWorld = function() {
     return 'Hello world !';
 }
 
-/**
- * On créé une nouvelle "application" express
- */
 const app = express()
 
-/**
- * On dit à Express que l'on souhaite parser le body des requêtes en JSON
- *
- * @example app.post('/', (req) => req.body.prop)
- */
 app.use(express.json())
 
-/**
- * On dit à Express que l'on souhaite autoriser tous les noms de domaines
- * à faire des requêtes sur notre API.
- */
 app.use(cors())
 
 
@@ -30,8 +19,5 @@ app.get('/hello', (req, res) => {
     res.send(helloWorld())
   })
 
-/**
- * On demande à Express d'ecouter les requêtes sur le port défini dans la config
- */
-app.listen(config.API_PORT, () => console.log('Running.'))
+app.listen(process.env.PORT, () => console.log('Running.'))
 
